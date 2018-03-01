3759433810112d91f530b295310903605fbd9e74
frankc60 <frankc60@gmail.com>
2018-03-01 20:17:28 +0000

    added inheritance of eventemitter

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-const EventEmitter = require('events');
+const events= require('events'),
+    util    = require("util");
 
-class MyEmitter extends EventEmitter {}
 
-const myEmitter = new MyEmitter();
+
+let myEmitter = new events.EventEmitter();
 
 myEmitter.on('SaveEvent', () => {
     console.log('saving event occured!');
 });
-
-
-
-
 
 const saveFile = () => {
 
@@ -21,3 +18,35 @@ const saveFile = () => {
 
 
 setTimeout(saveFile, 3000);
+
+//*************************************************************************** */
+
+//create student object, with property
+const Student = function(name) {
+    this.name = name;
+    this.doTest = (testName) => {
+        console.log(`doing the '${testName}' test!`);
+        //emit on this method.
+        this.emit("grade", 96);
+    }
+}
+
+//give Student object event emitters methods.
+util.inherits(Student, events.EventEmitter);
+
+
+//create a new instance of Student object - constructor
+let pete = new Student("pete");
+
+//call on method event emitter on Student object (from inheriting)
+pete.on("grade", (grade) => {
+    console.log(`${pete.name} has finished his test, 
+    and got graded with a '${grade}' mark.`);
+});
+
+
+//call method from object Parent (function)
+pete.doTest("aa");
+
+
+
