a7c8ac0a82564f2a62bf38efe52e13f0f455b865
frankc60 <frankc60@gmail.com>
2018-03-01 18:57:08 +0000

    working basic

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-//hello
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+
+myEmitter.on('SaveEvent', () => {
+    console.log('saving event occured!');
+});
+
+
+
+
+
+const saveFile = () => {
+
+    console.log("saving file simulation");
+    myEmitter.emit('SaveEvent');
+    console.log("finish.");
+};
+
+
+setTimeout(saveFile, 3000);
